1445aec839b3d8bc2de3e759da7ecb4fcbe218d1
muhannad_sph <muhannad.sk8@gmail.com>
2022-04-05 19:57:48 +0300

    Add imgur

---
+++ script.js
@@ -0,0 +1,53 @@
+
+const clientId = "712645836bdd60b";
+var defaultAlbumId = '';
+
+function requestAlbum() {
+    let albumId = document.getElementById("albumIdField").value;
+    if(!albumId) {
+        albumId = defaultAlbumId;
+    }
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            processAlbumRequest(req.responseText);
+        }
+        else if (req.readyState == 4 && req.status != 200) {
+            console.log(req.status + " Error with the imgur API: ", req.responseText);
+        }
+    }
+    req.open('GET', 'https://api.imgur.com/3/album/' + albumId + '/images', true); // true for asynchronous     
+    req.setRequestHeader('Authorization', 'Client-ID ' + clientId);
+    req.send();
+}
+
+function processAlbumRequest(response_text) {
+    var respObj = JSON.parse(response_text);
+    for (item of respObj.data.slice(0, 10)){
+        console.log(item)
+        requestImage(item.id);
+    }
+}
+
+function requestImage(imageHash) {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            processImageRequest(req.responseText);
+        }
+        else if (req.readyState == 4 && req.status != 200) {
+            console.log("Error with the imgur API");
+        }
+    }
+    req.open("GET", "https://api.imgur.com/3/image/" + imageHash, true); // true for asynchronous     
+    req.setRequestHeader('Authorization', 'Client-ID ' + clientId);
+    req.send();
+}
+
+function processImageRequest(response_text) {
+    var respObj = JSON.parse(response_text);
+    let imgElem = document.createElement("img");
+    imgElem.src = respObj.data.link;
+    //imgElem.referrerpolicy="no-referrer";
+    document.body.appendChild(imgElem);
+}
